93d4e158b59d621af9c78f42c8d25556eb351c6c
Akbar Syahputra Lubis <Akbar.Lubis@petrolink.com>
2022-04-19 10:48:35 +0700

    initial commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.util.Calendar.JANUARY;
+import static java.util.Calendar.OCTOBER;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student akbar = new Student(
+                    "Akbar",
+                    LocalDate.of(1994, 10, 7),
+                    "akbaryahputralubis@gmail.com"
+            );
+
+            Student hani = new Student(
+                    "Hani",
+                    LocalDate.of(1995, 11, 10),
+                    "hani@gmail.com"
+            );
+
+            repository.saveAll(
+                    List.of(akbar, hani)
+            );
+        };
+    }
+}
